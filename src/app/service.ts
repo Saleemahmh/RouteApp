33993a7e13fe6b3df0d00a2b9197a7d92616c965
Saleemahmh <ssaleemah24@gmail.com>
2017-07-10 13:05:21 +0530

    URL Updation

--- src/app/service.ts
+++ src/app/service.ts
@@ -4,23 +4,24 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class Service{
+     base_url:String= "http://mcsaleem01:8080/Routes-0.0.1-SNAPSHOT";
     cityname:any;
 constructor(private _http:Http){
     
 }
 getRoute(source:string, destination:string){
-    return this._http.get('http://mcsaleem01:8080/Routes-0.0.1-SNAPSHOT/bus/routenumbers?source='+source+'&destination='+destination)
+    return this._http.get(this.base_url + '/bus/routenumbers?source='+source+'&destination='+destination)
     .map(res => res.json());
 }
 getRoutes(){
-    return this._http.get('http://mcsaleem01:8080/Routes-0.0.1-SNAPSHOT/bus/routes').map(res => res.json());
+    return this._http.get(this.base_url+'/bus/routes').map(res => res.json());
 }
 getCitiesImg(city: string){
-    return this._http.get('http://mcsaleem01:8080/Routes-0.0.1-SNAPSHOT/cities/'+ city)
+    return this._http.get(this.base_url+'/cities/'+ city)
     .map(res=>res.json());
 }
 getTrainRoute(source:string, destination:string){
-    return this._http.get('http://mcsaleem01:8080/Routes-0.0.1-SNAPSHOT/train/time?source='+source+'&destination='+destination)
+    return this._http.get(this.base_url+'/train/time?source='+source+'&destination='+destination)
     .map(res => res.json());
 }
     updateCityname(cityname:string){
